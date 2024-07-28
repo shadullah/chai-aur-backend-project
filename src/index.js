8a693172b4148f0c443b8923031848161437ffd4
hexaalif <shadullahsad9@gmail.com>
2024-07-28 16:50:21 +0600

    utils code added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv";
-
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-  path: "./env",
+  path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongodb connection failed", err);
+  });
 
 /*
 import express from "express";
