2110e524d3df41f0edfdcef7ecdcb42dd6009ed3
hexaalif <shadullahsad9@gmail.com>
2024-08-10 15:43:19 +0600

    routes and controller code added

--- src/app.js
+++ src/app.js
@@ -16,4 +16,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+// app.use("/users", userRouter)
+app.use("/api/v1/users", userRouter); // industry use
+
 export { app };
